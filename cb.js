34593bdd049b447e11436b82266d2c1bdc842c3f
sarangamle <55396015+sarangamle@users.noreply.github.com>
2020-01-13 06:48:05 -0500

    Update cb.js

--- cb.js
+++ cb.js
@@ -2,11 +2,48 @@
 <html>
 <body>
 
-<script src="https://cdn.cai.tools.sap/webchat/webchat.js"
-channelId="2fbb9ee6-f5ef-45f3-b39f-57a3f9d28dce"
-token="2391633d07f83a329a7d345eee93378a"
-id="cai-webchat"
-></script>
+(function() { 
+	let template = document.createElement("template");
+	template.innerHTML = `
+		<style>
+		:host {
+			border-radius: 25px;
+			border-width: 4px;
+			border-color: black;
+			border-style: solid;
+			display: block;
+		} 
+		</style> 
+	`;
+
+	class ColoredBox extends HTMLElement {
+		constructor() {
+			super(); 
+			let shadowRoot = this.attachShadow({mode: "open"});
+			shadowRoot.appendChild(template.content.cloneNode(true));
+			this.addEventListener("click", event => {
+				var event = new Event("onClick");
+				this.dispatchEvent(event);
+			});
+			this._props = {};
+		}
+
+		onCustomWidgetBeforeUpdate(changedProperties) {
+			this._props = { ...this._props, ...changedProperties };
+		}
+
+		onCustomWidgetAfterUpdate(changedProperties) {
+			if ("color" in changedProperties) {
+				this.style["background-color"] = changedProperties["color"];
+			}
+			if ("opacity" in changedProperties) {
+				this.style["opacity"] = changedProperties["opacity"];
+			}
+		}
+	}
+
+	customElements.define("com-sap-sample-coloredbox", ColoredBox);
+})();
 
 </body>
 </html> 
